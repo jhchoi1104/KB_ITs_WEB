73d3f33402cb7e501df402e6a9b259432995ae53
Jonghyun Choi <wwhdgus112@gmail.com>
2024-05-28 18:51:37 +0900

    5월 28일 커밋

--- Vue05/chapter7/const.js
+++
@@ -1,3 +0,0 @@
-const p1 = { name: 'jojn', age: 20 };
-p1.age = 22;
-console.log(p1);
---
+++ Vue05/chapter7/test-vue-app/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h2>App 컴포넌트</h2>
+    <hr />
+    <ul>
+      <CheckboxItem />
+      <CheckboxItem />
+      <CheckboxItem />
+      <CheckboxItem />
+    </ul>
+  </div>
+</template>
+
+<script>
+import CheckboxItem from './components/CheckboxItem.vue';
+
+export default {
+  name: 'App',
+  components: { CheckboxItem },
+  data() {
+    return {
+      idols: [
+        { id: 1, name: 'BTS', checked: true },
+        { id: 2, name: 'Black Pink', checked: false },
+        { id: 3, name: 'EXO', checked: false },
+        { id: 4, name: 'ITZY', checked: false },
+      ],
+    };
+  },
+};
+</script>

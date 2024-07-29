e9f6aab2555382ee122048614723fd8aeef8884f
Jonghyun Choi <wwhdgus112@gmail.com>
2024-07-29 17:53:17 +0900

    7월 29일 커밋

---
+++ Jsp&Servlet/ex03/src/main/java/org/scoula/ex3/SportsServlet.java
@@ -0,0 +1,25 @@
+package org.scoula.ex3;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.IOException;
+
+@WebServlet("/sports")
+public class SportsServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // MIME 타입 설정
+        response.setContentType("text/html;charset=UTF-8");
+        
+        // 자바 I/O
+        PrintWriter out = response.getWriter();
+        
+        // html 작성 및 출력
+        out.print("<html><body>");
+        out.print("");
+        out.print("</body></html>");
+    }
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setCharacterEncoding("UTF-8");
+        doGet(request,response);
+    }
+}

692858cea5fd1ce188a736020ef4db9d7ae0ef9a
Jonghyun Choi <wwhdgus112@gmail.com>
2024-08-14 17:43:20 +0900

    8월14일 커밋 임

---
+++ Spring/boardapi/src/main/java/org/scoula/board/service/BoardServiceImpl.java
@@ -0,0 +1,98 @@
+package org.scoula.board.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j;
+import org.scoula.board.domain.BoardAttachmentVO;
+import org.scoula.board.domain.BoardVO;
+import org.scoula.board.dto.BoardDTO;
+import org.scoula.board.mapper.BoardMapper;
+import org.scoula.common.util.UploadFileName;
+import org.scoula.common.util.UploadFiles;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
+@Log4j
+@Service
+@RequiredArgsConstructor
+public class BoardServiceImpl implements BoardService{
+    final private BoardMapper mapper;
+    private final static String BASE_DIR = "c:/upload/board";
+
+    @Override
+    public List<BoardDTO> getList() {
+        log.info("getList................");
+
+        return mapper.getList().stream()
+                .map(BoardDTO::of)
+                .toList();
+    }
+
+    @Override
+    public BoardDTO get(Long no) {
+        log.info("get............."+no);
+
+        BoardDTO board = BoardDTO.of(mapper.get(no));
+        return Optional.ofNullable(board)
+                .orElseThrow(NoSuchElementException::new);
+    }
+
+    private void upload(Long bno, List<MultipartFile> files) {
+        for (MultipartFile part: files) {
+            if(part.isEmpty()) continue;
+            try {
+                String uploadPath = UploadFiles.upload(BASE_DIR, part);
+                BoardAttachmentVO attach = BoardAttachmentVO.of(part,bno,uploadPath);
+                mapper.createAttachment(attach);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    @Transactional
+    @Override
+    public void create(BoardDTO board) {
+        log.info("create............"+ board);
+
+        BoardVO boardVO = board.toVO();
+        mapper.create(boardVO);
+
+        List<MultipartFile> files = board.getFiles();
+        if(files != null && !files.isEmpty()) {
+            upload(boardVO.getNo(), files);
+        }
+    }
+
+    @Override
+    public boolean update(BoardDTO board) {
+        log.info("update......."+board);
+
+        return mapper.update(board.toVO()) == 1;
+    }
+
+    @Override
+    public boolean delete(Long no) {
+        log.info("delete......." + no);
+
+        return mapper.delete(no) == 1;
+    }
+
+    @Override
+    public BoardAttachmentVO getAttachment(Long no) {
+        return mapper.getAttachment(no);
+    }
+
+    @Override
+    public boolean deleteAttachment(Long no) {
+        return mapper.deleteAttachment(no)==1;
+    }
+
+
+}
